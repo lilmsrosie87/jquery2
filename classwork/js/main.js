e8ae5a7b7cec075177e8964429bbbdbdd76b7183
Roslyn Harris <rharris0113@gmail.com>
2017-12-12 21:26:43 -0800

    still working on adding ordered list

---
+++ classwork/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".fadeOut").click(function(){
+		$("img").hide();
+	});
+});
+
+$(document).ready(function(){
+	$(".fadeIn").click(function(){
+		$("img").show();
+	});
+});
