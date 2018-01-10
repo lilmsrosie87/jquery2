4ea733217d2b92956825774a338c96e89b65608b
Roslyn Harris <rharris0113@gmail.com>
2018-01-09 20:36:03 -0800

    working on beginner JS

--- classwork/js/main.js
+++ classwork/js/main.js
@@ -1,3 +1,45 @@
+// saving user name (Week 8 group discussion)
+// var username = document.getElementById("username");
+
+// function showName() {
+	// console.log(username.value);
+// }
+
+// Week 8 group discussion #4
+
+var students = [
+
+	{
+		name: "Lisa",
+		age: 12,
+		gender: "female",
+	},
+	
+	{
+		name: "Jose",
+		age: 8,
+		gender: "male",
+	},
+	
+	{
+		name: "Rosie",
+		age: 15,
+		gender: "female",
+	},
+]
+	
+for (var i = 0; < students.length; i++) {
+	console.log(students[i].name);
+	console.log(students[i].age);
+}
+
+// group discussion #5
+
+var stunnaBitch = document. getElementById("students");
+
+for (var i = 0; 
+
+
 $(document).ready(function(){
 	$(".fadeOut").click(function(){
 		$("img").hide();
