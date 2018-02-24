1a1a2983ca19c6543ab4fb087bc67b4055a40c81
Roslyn Harris <rharris0113@gmail.com>
2018-02-23 20:28:18 -0800

    working on completing week 8 questions

--- homework/js/excercise.js
+++ homework/js/excercise.js
@@ -1,4 +1,15 @@
-// Week 8 homework excercises
+// Week 8 homework exercises
+
+// In class questions
+// 1. In plain English, explain what document.getElementById() means.
+// returns the element associated with that ID
+
+// 2. In simple terms, what does a ‘for loop’ do?
+// loops thru a block of code (executing code) a number of times
+
+// 3. What is a variable?
+// A variable is a place holder for something
+
 
 // excercise #5
 
